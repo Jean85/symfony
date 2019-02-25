1e3db4bf8f9fa764d1f46f66739c847365f53dd5
Alessandro Lai <alessandro.lai85@gmail.com>
2019-02-26 00:32:42 +0100

    ContainerInterface::set accepts callable

--- src/Symfony/Component/DependencyInjection/ContainerInterface.php
+++ src/Symfony/Component/DependencyInjection/ContainerInterface.php
@@ -33,8 +33,8 @@ interface ContainerInterface extends PsrContainerInterface
     /**
      * Sets a service.
      *
-     * @param string $id      The service identifier
-     * @param object $service The service instance
+     * @param string          $id      The service identifier
+     * @param object|callable $service The service instance
      */
     public function set($id, $service);
 
